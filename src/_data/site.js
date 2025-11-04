c68b1419e6241bd2ad10f554fa904389c2511bd6
Makeademic <kaplanwizard@gmail.com>
2025-11-04 18:21:38 +0000

    update relative to absolute URL in site.js and default.njk

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'db',
-  shortDescription: "Database of database for programmable keyboards",
-  description: "db is a public and open-source online database for database of programmable keyboards, with a focus on QMK ergonomic mechanical keyboards. You can filter user-submitted database like Miryoku by key count, firmware, keyboard, stagger, languages, OS, home row mods, and more!",
-  keywords: ["QMK", "keyboard", "database", "remapping", "database", "open-source", "mechanical keyboards", "ergomech", "keycap", "custom", "customization", "ZMK", "typing", "touchtyping", "macros", "inspiration", "ideas", "QWERTY", "Colemak", "Dvorak", "keyboard layout"],
+  title: '11ty Tailwind Database',
+  shortDescription: "Digital Database using Eleventy and Tailwind CSS",
+  description: "Template for Digital Databases",
+  keywords: ["11ty template"],
   // how many posts you want to show for each page
   paginate: 18,
   // WARNING: This assumes that the pathPrefix is "/"
