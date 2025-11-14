c1bc6e24d84c053d8ae93dfa90c3abee8352a98b
Makeademic <kaplanwizard@gmail.com>
2025-11-14 14:04:29 -0500

    try updating line 28 with pathprefix

--- src/assets/js/card.js
+++ src/assets/js/card.js
@@ -25,7 +25,7 @@ function card(post, postUrl) {
           <a href="${post.url}">
           <img
           class="w-full m-0 rounded-t lazy max-h-72 object-cover object-top card-thumbnail"
-          src="${post.databaseImage}"
+          src="/11tyTailwindDatabase/${post.databaseImage}"
           width="960"
           height="500"
           alt="${post.imageAlt}">
