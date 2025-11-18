2aa241c02f9187c67ebfec508151c3578919776f
Makeademic <kaplanwizard@gmail.com>
2025-11-18 13:58:22 -0500

    change site title and logo path

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: '11ty Tailwind Database',
+  title: 'BASIRA: Books as Symbols in Renaissance Art',
   shortDescription: "Digital Database using Eleventy and Tailwind CSS",
   description: "Template for Digital Databases",
   keywords: ["11ty template"],
   // how many posts you want to show for each page
   paginate: 18,
   // WARNING: This assumes that the pathPrefix is "/"
-  logo: '/assets/svg/logo/light.svg'
+  logo: 'https://makeademic.github.io/11tyTailwindDatabase/assets/img/BASIRAlogo.jpg'
 };
